e2a9fcacb613d1dc9f1346b7e36da956670bfc79
Chetan <chetan.aiofficial@gmail.com>
2023-09-01 19:24:27 +0530

    image source

--- django_app/views.py
+++ django_app/views.py
@@ -10,34 +10,12 @@ import tensorflow as tf
 import numpy as np
 
 def index(request):
-    if request.method == 'POST':
-        """
-        Django Image API
-        """
-        file = request.FILES['imageFile']
-        file_name = default_storage.save(file.name, file)
-        file_url = default_storage.path(file_name)
+    return render(request, 'index.html')
 
-        #
-        # Load and preprocess the image
-        #
-        image = load_img(file_url, target_size=(224, 224))
-        numpy_array = img_to_array(image)
-        image_batch = np.expand_dims(numpy_array, axis=0)
+def prediction(request):
+    
+    file = request.FILES['imageFile']
+    file_name = default_storage.save(file.name, file)
+    file_url = default_storage.url(file_name)
 
-        processed_image = vgg16.preprocess_input(image_batch.copy())
-
-        # #
-        # # Get the predicted probabilities
-        # #
-        predictions = settings.IMAGE_MODEL.predict(processed_image)
-
-        top_predictions = decode_predictions(predictions, top=5)
-
-        # Define the path to the image with bounding boxes
-        bounded_image_path = 'path_to_bounded_image.jpg'  # Update with the actual path
-        
-        return render(request, "index.html", {"predictions": top_predictions[0], "image_path": bounded_image_path})
-    else:
-        return render(request, 'index.html')
-            
+    return render(request, 'prediction.html', {"file_path": file_url})
--- django_model/urls.py
+++ django_model/urls.py
@@ -2,11 +2,12 @@ from django.urls import path,include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_app.urls')),
+    path('prediction', views.prediction, name='prediction'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
